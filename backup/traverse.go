a34bf644064e1c50473568efd731da2342cc6264
viveknathani <viveknathani2402@gmail.com>
2021-07-26 00:37:37 +0530

    refactor: separate out operations
    This commit brings in the refactored methods. The public API functions
    are now shorter. Their work is taken outside. This can help in future
    if the process of backing up a single file is changed.

--- backup/traverse.go
+++ backup/traverse.go
@@ -8,8 +8,7 @@ func TraverseAndDoEncryptedBackup(source string, destination string) {
 	if isDirectory(source) {
 
 		dirName := extractName(source)
-		makeDirectory(destination, dirName)
-		createMetaIfNotFound(destination + dirName)
+		setupDirectory(destination, dirName)
 		contents := getDirectoryListing(source)
 
 		for _, element := range contents {
@@ -19,14 +18,7 @@ func TraverseAndDoEncryptedBackup(source string, destination string) {
 		return
 	}
 
-	cs := getCheckSum(source)
-	metaData := newMetaData(cs, source)
-
-	if !metaExists(metaData, destination) {
-		addToMeta(metaData, destination)
-		encrypted := encryptFile(source)
-		writeToDisk(destination, encrypted)
-	}
+	encryptAndBackup(source, destination)
 }
 
 // TraverseAndDecryptBackup will recursively traverse the source path
@@ -44,6 +36,41 @@ func TraverseAndDecryptBackup(source string, destination string) {
 
 		return
 	}
+
+	decryptAndRestore(source, destination)
+}
+
+// setupDirectory will do create the directory along with its
+// .meta file
+func setupDirectory(destination string, dirName string) {
+
+	makeDirectory(destination, dirName)
+	createMetaIfNotFound(destination + dirName)
+}
+
+// encryptAndBackup will encrypt the given file and write to disk
+func encryptAndBackup(source string, destination string) {
+
+	fileName := extractName(source)
+	cs := getCheckSum(source)
+	metaData := createMetaData(fileName, cs)
+	fileExists, checkSumExists := checkMetaExistence(metaData, destination)
+
+	if fileExists && checkSumExists {
+		return
+	}
+
+	if fileExists && !checkSumExists {
+		updateMeta(metaData, destination)
+		removeFile(destination)
+	}
+
+	encrypted := encryptFile(source)
+	writeToDisk(destination, encrypted)
+}
+
+// decryptAndRestore will decrypt the given file and write to disk
+func decryptAndRestore(source string, destination string) {
 
 	decrypted := decryptFile(source)
 	writeToDisk(destination, decrypted)
