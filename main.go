d0bc983adc3f372ddd3f90375af397482f795b06
viveknathani <viveknathani2402@gmail.com>
2021-07-25 23:05:13 +0530

    feat: consume the backup API, add usage
    This commit brings in the function that displays the usage of this
    project, along with the procedure to run the program.

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/rwxLife/alike/backup"
+)
+
+// usage will print the information about the 'alike' command
+func usage() {
+
+	fmt.Println("Usage: alike OPERATION SOURCE DESTINATION")
+	fmt.Println("Possible values for, \"OPERATION\": backup, restore")
+	fmt.Println("Flags")
+	fmt.Println("--help: display this help and exit")
+}
+
 func main() {
 
+	argLength := len(os.Args)
+
+	// To deal with typos and/or ignorance
+	if argLength == 1 ||
+		os.Args[1] == "--help" ||
+		argLength != 4 ||
+		!(os.Args[1] == "backup" || os.Args[1] == "restore") {
+		usage()
+		return
+	}
+
+	operation := os.Args[1]
+	source := os.Args[2]
+	destination := os.Args[3]
+
+	if operation == "backup" {
+		backup.TraverseAndDoEncryptedBackup(source, destination)
+		return
+	}
+
+	backup.TraverseAndDecryptBackup(source, destination)
 }
